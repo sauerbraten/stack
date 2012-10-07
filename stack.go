bf97e088f018edbe92864d180234ea98c952a57b
sauerbraten <willing.alexander@googlemail.com>
2012-10-07 21:05:01 +0200

    added size field to stack type; Size() now simply returns that field, this eliminiates the need to walk down the whole stack.

--- stack.go
+++ stack.go
@@ -8,12 +8,13 @@ type element struct {
 
 // A stack.
 type Stack struct {
-	top *element
+	top  *element
+	size int
 }
 
 // New returns an initialized stack.
 func New() *Stack {
-	return &Stack{nil}
+	return &Stack{nil, 0}
 }
 
 // Push puts a new element on top of the stack.
@@ -21,6 +22,7 @@ func (s *Stack) Push(value interface{}) {
 	temp := s.top
 	newE := &element{temp, value}
 	s.top = newE
+	size++
 }
 
 // Pop removes the top element and returns it.
@@ -31,25 +33,11 @@ func (s *Stack) Pop() interface{} {
 
 	v := s.top.value
 	s.top = s.top.next
+	size--
 	return v
 }
 
 // Size returns the amount of elements in the stack.
 func (s *Stack) Size() int {
-	i := 0
-
-	// if stack empty
-	if s.top == nil {
-		return i
-	}
-
-	e := s.top
-	i++
-
-	for e.next != nil {
-		e = e.next
-		i++
-	}
-
-	return i
+	return s.size
 }
