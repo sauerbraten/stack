5b233871f7ecb0696bef76d7e66738fab37b5767
sauerbraten <willing.alexander@googlemail.com>
2013-01-16 19:06:30 +0100

    added package comment

--- stack.go
+++ stack.go
@@ -1,23 +1,24 @@
+// A simple minimalistic stack, built on a slice.
 package stack
 
 import (
 	"errors"
 )
 
-// A stack.
+// A stack
 type Stack []interface{}
 
-// New returns an initialized stack.
+// New returns an initialized stack
 func New() *Stack {
 	return new(Stack)
 }
 
-// Push puts a new element on top of the stack.
+// Push puts a new element on top of the stack
 func (s *Stack) Push(value interface{}) {
 	*s = append(*s, value)
 }
 
-// Pop removes the top element and returns it, along with an error that is only != nil if the stack is empty.
+// Pop removes the top element and returns it, along with an error that is only != nil if the stack is empty
 func (s *Stack) Pop() (interface{}, error) {
 	if len(*s) == 0 {
 		return nil, errors.New("stack is empty")
@@ -30,7 +31,7 @@ func (s *Stack) Pop() (interface{}, error) {
 	return temp, nil
 }
 
-// Size returns the amount of elements in the stack.
+// Size returns the amount of elements in the stack
 func (s *Stack) Size() int {
 	return len(*s)
 }
